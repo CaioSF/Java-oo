d782f52c188e254dc62e0c4b10a799e0ed90121b
Pedro Sudré <pedrosudre04@gmail.com>
2022-12-03 00:00:27 -0300

    classe cargo

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Funcionario extends Pessoa {
     private Double salario;
+
+    private Collection<Cargo> cargos = new ArrayList<Cargo>();
 
     public Double getSalario() {
         return salario;
@@ -9,8 +14,13 @@ public class Funcionario extends Pessoa {
         this.salario = salario;
     }
 
+    public void addCargo(Cargo cargo) {
+        this.cargos.add(cargo);
+    }
+
     @Override
     public void autenticar() {
         System.out.println("Usuário Autenticado! =)");
     }
+
 }
